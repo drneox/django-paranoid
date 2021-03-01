05adcd68d9114b5727ef8a4ca2ad9fae3553b20b
carlos <cganozap@gmail.com>
2021-03-01 01:30:08 -0500

    publish CI

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name="django-paranoid",
       description="this library adds 'created_at', 'updated_at' and 'delete_at'  fields like a rail apps in django,"
                   " also added soft delete method",
       long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
       author="Carlos Ganoza Plasencia",
       author_email="cganozap@gmail.com",
       url="https://github.com/drneox/django-paranoid",
